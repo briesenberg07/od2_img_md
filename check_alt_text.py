94ac7469a7c34a8d2870bcae095f2ba9e7869375
Cole Herman <colesten@gmail.com>
2026-07-21 16:19:39 -0700

    Fix check alt text to use correct fields. Remove pyexiftool, too complicated for this

--- check_alt_text.py
+++ check_alt_text.py
@@ -1,28 +1,45 @@
 from pathlib import Path
+from csv import DictReader
 from subprocess import run
-import exiftool
 import os
+import json
 
 def main():
-    # Get path from user for files/ folder
+    # Get path from user
     path_input = input("Enter the path to the files folder containing images: ")
     path = Path(path_input)
 
-    # Make files list
-    files = [entry.path for entry in os.scandir(path) if entry.is_file() and entry.name.lower().endswith((".tif", ".tiff"))]
+    # Build files list from files/ folder
+    files = [
+        entry.path
+        for entry in os.scandir(path)
+        if entry.is_file() and entry.name.lower().endswith((".tif", ".tiff"))
+    ]
     if not files:
         print("No files found")
         return
 
-    # Read file(s) metadata
-    with exiftool.ExifToolHelper() as et:
-        metadata = et.get_tags(files, tags=["XMP-iptcCore:AltTextAccessibility", "ImageDescription"]) # No leading - in tags here, unlike command line
-        # Print alt text and image description field values for each image
-        for d in metadata:
-            print("File: ", d.get("SourceFile"))
-            print("Alt Text: ", d.get("XMP-iptcCore:AltTextAccessibility", "MISSING"))
-            print("Image Description: ", d.get("ImageDescription", "MISSING"))
-        #FIXME: save missing ones to summarize at bottom
+    # Check each file for alt text
+    for file_path in files:
+        result = run(
+            [
+                "exiftool",
+                "-j",
+                "-EXIF:ImageDescription",
+                "-XMP:AltTextAccessibility",
+                "-XMP-iptcCore:AltTextAccessibility",
+                file_path,
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        # Print results
+        data = json.loads(result.stdout)[0]
+        print("File:", data.get("SourceFile"))
+        print("ImageDescription:", data.get("ImageDescription", "MISSING"))
+        print("AltTextAccessibility:", data.get("AltTextAccessibility", "MISSING"))
 
 if __name__ == "__main__":
     main()
